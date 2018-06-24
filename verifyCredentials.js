c293a5386d88f03bfb5c38b98820aa269d22f446
Kirill Levitskiy <kirill.levitskiy@gmail.com>
2018-06-24 19:35:46 +0300

    return true for verify script

--- verifyCredentials.js
+++ verifyCredentials.js
@@ -12,23 +12,6 @@ module.exports = verify;
  * @returns Promise sending HTTP request and resolving its response
  */
 function verify(credentials) {
-
-    // access the value of the apiKey field defined in credentials section of component.json
-    const apiKey = credentials.apiKey;
-
-    if (!apiKey) {
-        throw new Error('API key is missing');
-    }
-
-    // sending a request to the most simple endpoint of the target API
-    const requestOptions = {
-        uri: 'https://petstore.elastic.io/v2/user/me',
-        headers: {
-            'api-key': apiKey
-        },
-        json: true
-    };
-
-    // if the request succeeds, we can assume the api key is valid
-    return request.get(requestOptions);
+    
+    return true;
 }
