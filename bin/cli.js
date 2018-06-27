60cac12ee6ac146e6ae650c6301415190adae239
Kirill <kyrylo.levytskyi@elastic.io>
2018-06-27 11:37:31 +0300

    test java12

--- bin/cli.js
+++ bin/cli.js
@@ -1,6 +1,3 @@
-#!/usr/bin
+#!/usr/bin/env node
 
-/usr/bin/sh ./compile-java-code.sh
-/usr/bin/echo "compile-java-code - done"
-/usr/bin/sh ./compile-java8-code.sh
-/usr/bin/echo "compile-java8-code - done"
+console.log("hello cli")
